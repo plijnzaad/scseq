dab794b16c31844a6c28ac387d77add7f1b902e4
Philip Lijnzaad <plijnzaad@gmail.com>
2016-04-01 12:41:03 +0200

    wrong lib loc

--- gtf2fa.pl
+++ gtf2fa.pl
@@ -2,7 +2,7 @@
 
 #use lib "/Users/d.grun/data/bin";
 # use lib "/home/d.grun/bin";
-use lib "/Users/abelvertesy/Mount_points/X_React/05.5.Reference_generation/";
+# use lib "/Users/abelvertesy/Mount_points/X_React/05.5.Reference_generation/";
 use tools;
 
 
