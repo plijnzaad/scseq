83937b9ed682daae19c514dca8fdd9c27c042625
Philip Lijnzaad <plijnzaad@gmail.com>
2016-09-16 16:44:52 +0200

    *** empty log message ***

--- ucsc2gtf.pl
+++ ucsc2gtf.pl
@@ -13,8 +13,8 @@ use tools;
 ### for part of the other half of a diploid genome), as they often have the same genes mapping there.
 ### Looks like the --utr=3,5 only outputs the 3 or 5' UTRs, in weird format: 
 #### ---->>>>------>>>>------====  (--- : untransl exon, >>>: intron, ===: translated exon)
-#### |-----------------------| is transcript coordinates (should have been called 5UTR or 3UTR)
-####                   |-----| is the untranslated part of the first exon (unclear what to call this)
+#### |<- - - - - - - - - - ->| is transcript coordinates (should have been called 5UTR or 3UTR)
+####                   |<- ->| is the untranslated part of the first exon (unclear what to call this)
 
 if (scalar @ARGV == 1) {
     die "usage: -in=INPUT.ucsc_format -out=OUTPUT.gtf -m=gene2isoforms.tsv -utr=0,3,5\n" if ($ARGV[0] eq "help");
