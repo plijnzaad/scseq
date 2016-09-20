cfdf019b9eb67b887bf5992a8a2a5aa99532f2e2
Philip Lijnzaad <plijnzaad@gmail.com>
2016-09-20 12:27:19 +0200

    no use lib (use env.sh); Unix line endings; using new calling convention; description on top. Untested

--- 04.mask_polyA.pl
+++ 04.mask_polyA.pl
@@ -1,21 +1,20 @@
 #!/usr/bin/perl -w -s
 
-use lib "/Users/abelvertesy/x_reactivation/analysis/DevCell_analysis/06.5.Reference_generation";  	# use lib '/hpc/hub_oudenaarden/bin/';
-use tools;
+## replace any run of A's longer than n (default 10) with as many N's
 
+use tools;
 
 if (scalar @ARGV == 1){
     die "usage: -in=input.fa -n=number of As (default: 10) -out=output.fa\n" if $ARGV[0] eq "help";
 }
 
 $n = 10 if !$n;
-%seqs = ();
-fasta2hash(\%seqs,$in);
+$seqs = fasta2hash($in);
 $m = "(A|a){".$n.",}";
 
-open(OUT,">",$out);
-foreach $k (keys %seqs){
-  $s = $seqs{$k};
+open(OUT,">",$out) or die "$out:$!";
+foreach $k (keys %$seqs){
+  $s = $seqs->{$k};
   while ( $s =~ /$m/g ){
     $x = $&;
     $x =~ s/[Aa]/N/g;
