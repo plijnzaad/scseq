3eab9a85d440da9d7810ae59723088e332061f4a
Philip Lijnzaad <plijnzaad@gmail.com>
2016-09-21 14:27:09 +0200

    revamped command-line parsing, and now using Bio::SeqIO for fasta reading and writing

--- mask_polyA.pl
+++ mask_polyA.pl
@@ -1,26 +1,42 @@
-#!/usr/bin/perl -w -s
+#!/usr/bin/perl -w
 
 ## replace any run of A's longer than n (default 10) with as many N's
 
-use tools;
+use strict;
 
-if (scalar @ARGV == 1){
-    die "usage: -in=input.fa -n=number of As (default: 10) -out=output.fa\n" if $ARGV[0] eq "help";
-}
+use Getopt::Long;
+use Bio::SeqIO;
 
-$n = 10 if !$n;
-$seqs = fasta2hash($in);
-$m = "(A|a){".$n.",}";
+my($in, $out, $n, $help);
+$n=10;
 
-open(OUT,">",$out) or die "$out:$!";
-foreach $k (keys %$seqs){
-  $s = $seqs->{$k};
-  while ( $s =~ /$m/g ){
-    $x = $&;
+my $usage="Usage: mask_polyA.pl --in input.fa --out output.fa  -n number of As (default: 10)\n" ;
+
+GetOptions('i|in=s' => \$in,
+           'o|out=s'=> \$out,
+           'n=i'    => \$n,
+           'h|help' => \$help
+    ) || die $usage;
+
+die $usage if $help || !$in  || !$out ;
+
+my $regexp = "([Aa]{$n,})";
+
+my $input  = Bio::SeqIO->new(-file => $in , '-format' => 'fasta');
+my $output = Bio::SeqIO->new(-file=> "> $out", '-format' => 'fasta');
+
+while ( my $seq=$input->next_seq()  ) { 
+  my $s=$seq->seq();
+
+  ## $s= join("", map { $_ = 'N' x length($_) if /$regexp/; $_}  split(/$regexp/, $s)); # slightly slower
+
+  while ( $s =~ /$regexp/g ){
+    my $x = $&;
     $x =~ s/[Aa]/N/g;
     $s = substr($s,0,pos($s) - length($x)).$x.substr($s,pos($s),length($s) - pos($s));
   }
-  print OUT ">".$k."\n".$s."\n";
+
+  $seq->seq($s);
+  $output->write_seq($seq);
 }
-close(OUT);
 
