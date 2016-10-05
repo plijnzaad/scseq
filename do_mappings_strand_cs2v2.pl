0e3daf60143022f2fe375b9eb2382f244456ddaf
Philip Lijnzaad <plijnzaad@gmail.com>
2016-10-05 15:11:30 +0200

    initial merge with do_mappings_strand.pl, commit 1e78afb703f49087c7bd1a587c8a743d3ac61cff (Wed Oct 5 14:19:25 2016 +0200)

--- do_mappings_strand_cs2v2.pl
+++ do_mappings_strand_cs2v2.pl
@@ -1,10 +1,40 @@
 #!/usr/bin/perl -w -s
-use lib '/hpc/hub_oudenaarden/bin/';
 use tools;
 
 if (!($r && $f1 && $out && $t)){
-    die "usage: -r=REFERENCE -f1=READ1 -f2=READ2(optional) -out=OUTPUT_PREFIX -outdir=OUTPUT_DIRECTORY (optional) -t=THREADS -ind=is or bwtsw (default: is (<2GB reference) -q=PHRED_QUALITY_FOR_TRIMMING (0..62) -aln_n=edit distance -aln_k=edit distance in seed -l=SEED_LENGTH -BL=barcode length left read (bwa aln option -B, default is 0) -BR=barcode length right read (bwa aln option -B, default is 0) -i= 1 or 0 (1 if indexing is required) -gff=DATA.gff (optional, produces wig files) -s_flag=1 (optional: strand specific mapping) -u=1 (optional: only reads mapping to one strand) -uniq=1 (optional, keep only uniquely mapped reads) -test=1 -npr= 0 or 1 (1: do not process_sam.pl 2: run only do not process_sam.pl ) -nsam= 0 or 1 (1: do not produce new sam file) -cel=0 or 1 (1: process CEL-seq *.sam output) -bar=cel-seq_barcodes.csv -fstr= 1 or 0 ( if 1 only mappings to the sense strand are allowed ) -anno= anno.csv ( optional (CEL-seq), when mapping to the genome, run get_anno.pl on sam file first) -rb= 0 or 1 (optional (CEL-seq), use random barcodes) -rb_len= length of random barcode (default = 4) -dprm= 0 or 1 (for CEL-seq: 1: reomve pcr duplicates)\n";
+    die "usage:  -r=REFERENCE     \
+                 -f1=READ1    \
+                 -f2=READ2 (optional)    \
+                 -out=OUTPUT_PREFIX    \
+                 -outdir=OUTPUT_DIRECTORY (optional)    \
+                 -t=THREADS  (0)  \
+                 -ind=is or bwtsw (default: is (<2GB reference)    \
+                 -q=PHRED_QUALITY_FOR_TRIMMING (0..62, default 0)   \
+                 -aln_n=edit distance  (bwa aln option -n, default 0.04)  \
+                 -aln_k=edit distance in seed  (bwa aln option -k, default 2)   \
+                 -l=SEED_LENGTH  (bwa -l option)  \
+                 -BL=barcode length left read (bwa aln option -B, default is 0)    \
+                 -BR=barcode length right read (bwa aln option -B, default 0)   \
+                 -i= 1 or 0 (1 if indexing is required, runs bwa index )    \
+                 -gff=DATA.gff (optional, produces wig ?? files. Passed to process_sam_cel_seq.pl  )    \
+                 -s_flag=1 (optional: strand specific mapping. Passed to process_sam_cel_seq.pl)    \
+                 -u=1 (optional: only reads mapping to one strand. Passed to process_sam_cel_seq.pl )    \
+                 -uniq=1 (optional, keep only uniquely mapped reads. Passed to process_sam_cel_seq.pl )    \
+                 -npr=0,1,2: 0: map and process; 1: only map ; 2: only process
+                 -nsam= 0 or 1 (1: do *not* produce new sam file (calls bwa samse/sampe)    \
+                 -cel=0 or 1 (1: call process_sam_cel_seq.pl; 0: call process_sam_strand.pl (obsolete) \
+                 -bar=cel-seq_barcodes.csv    \
+                 -fstr= 1 or 0 ( if 1 only mappings to the sense strand are allowed; passed to process_sam_cel_seq.pl )    \
+                 -anno= anno.csv ( optional (CEL-seq), when mapping to the genome, run get_anno.pl on sam file first)    \
+                 -rb= 0 or 1 (optional (CEL-seq), use random barcodes. Passed to process_sam_cel_seq.pl)    \
+                 -s_flag= 0 or 1 (separate output for sense and antisense hits, passed to process_sam_cel_seq.pl)  \
+                 -rb_len= length of random barcode (default = 4), passed to process_sam_cel_seq.pl    \
+                 -dprm= 0 or 1 (for CEL-seq: 1: remove pcr duplicates, passed to process_sam_cel_seq.pl) \
+                 -test=0 or 1 (latter runs in test mode, doesn't call external programs) \
+                 -cel384=0 or 1 (if 1, run the 384 well version)   
+";
 }
+
 # hard coded:
 $n = 100; # maximal number of unpaired reads to output in XA tag
 $N = 100; # maximal number of paired reads to output in XA tag
@@ -24,7 +54,7 @@ $rb_len = 4 if !$rb_len;
 $dprm = 0 if !$dprm;
 $cel384 = 0 if !$cel384;
 $rb_len = 6 if $cel384;
-$BR = 8+$rb_len if $cel384;
+$BR = 8+$rb_len if $cel384; # hard-coded !?!?
 $aln_n = 0.04 if !$aln_n; # edit distance
 $aln_k = 2 if !$aln_k; # edit distance in seed
 
@@ -34,19 +64,21 @@ if ($outdir){
     $out = $outdir."/".$out;
 }
 $pflag = 0;
-$pflag = 1 if ($f1 && $f2);
+$pflag = 1 if ($f1 && $f2);             # paired
 
 
 if ($cel384){
-	$str = "perl /hpc/hub_oudenaarden/lennart/add_bc_to_R2.pl -fastq=".$f1.",".$f2." -rb_len=".$rb_len;
-   print $str."\n";
-    (system($str) == 0 or die "Could not execute ".$str."\n") if ($test == 0);
+  $str = "add_bc_to_R2.pl -fastq=$f1,$f2 -rb_len=$rb_len";
+  print $str."\n";
+  execute(cmd=>$str, merge=>1) if ($test == 0);
 }
 
+die "reference genome $r not found" unless -f $r;
+
 if ($i){
-    $str = "/hpc/hub_oudenaarden/bin/software/bwa-0.6.2/bwa index -a ".$ind." ".$r;
+    $str = "bwa index -a $ind $r";
     print $str."\n";
-    (system($str) == 0 or die "Could not execute ".$str."\n") if ($test == 0);
+    execute(cmd=>$str, merge=>1) if ($test == 0);
 }
 
 @F = ($f1);
@@ -67,7 +99,7 @@ if ( $npr != 2 ){
 		if ($F[$i] =~ /txt/){
 	    	$str = "qseq2fastq.pl -clean=1 -in=".$F[$i]." > ".$G[$i];
 	    	print $str."\n";
-	    	(system($str) == 0 or die "Could not execute ".$str."\n") if ($test == 0);
+                execute(cmd=>$str) if $test==0;
 		}
  		if ( $i == 0 ) {
             $B = $BL;
@@ -75,30 +107,30 @@ if ( $npr != 2 ){
             $B = $BR;
         }
         if ( $cel384 == 0){
-            $str = "/hpc/hub_oudenaarden/bin/software/bwa-0.6.2/bwa aln -B ".$B." -q ".$q." -n ".$aln_n." -k ".$aln_k." -l ".$l." -t ".$t." ".$r." ".$G[$i]." > ".$H[$i];
+            $str = "bwa aln -B ".$B." -q ".$q." -n ".$aln_n." -k ".$aln_k." -l ".$l." -t ".$t." ".$r." ".$G[$i]." > ".$H[$i];
             print $str."\n";
-            (system($str) == 0 or die "Could not execute ".$str."\n") if ($test == 0);
+            execute(cmd=>$str, merge=>0) if ($test == 0);
         }
         if ( $cel384 == 1 && $i == 1){
-            $str = "/hpc/hub_oudenaarden/bin/software/bwa-0.6.2/bwa aln -B ".$B." -q ".$q." -n ".$aln_n." -k ".$aln_k." -l ".$l." -t ".$t." ".$r." ".$K[$i]." > ".$H[$i];
+            $str = "bwa aln -B ".$B." -q ".$q." -n ".$aln_n." -k ".$aln_k." -l ".$l." -t ".$t." ".$r." ".$K[$i]." > ".$H[$i];
             print $str."\n";
-            (system($str) == 0 or die "Could not execute ".$str."\n") if ($test == 0);
+            execute(cmd=>$str, merge=>0) if ($test == 0);
         }
     }
     
     if ( $nsam == 0 ){
         if ($pflag && $cel384 == 0){
-            $str = "/hpc/hub_oudenaarden/bin/software/bwa-0.6.2/bwa sampe -n ".$n." -N ".$N." ".$r." ".join(" ",(@H, @G))." > ".$out.".sam";
+            $str = "bwa sampe -n ".$n." -N ".$N." ".$r." ".join(" ",(@H, @G))." > ".$out.".sam";
         }elsif ($pflag && $cel384 == 1){
-            $str = "/hpc/hub_oudenaarden/bin/software/bwa-0.6.2/bwa samse -n ".$n." ".$r." ".join(" ",(@H[1], @K[1]))." > ".$out.".sam";
+            $str = "bwa samse -n ".$n." ".$r." ".join(" ",(@H[1], @K[1]))." > ".$out.".sam";
         }else{
-            $str = "/hpc/hub_oudenaarden/bin/software/bwa-0.6.2/bwa samse -n ".$n." ".$r." ".join(" ",(@H, @G))." > ".$out.".sam";
+            $str = "bwa samse -n ".$n." ".$r." ".join(" ",(@H, @G))." > ".$out.".sam";
         }
     }
     print $str."\n";
-    (system($str) == 0 or die "Could not execute ".$str."\n") if ($test == 0);
-    
-}
+    execute(cmd=>$str) if ($test == 0);
+}                                       # npr!=2
+
 if ( $npr == 0 || $npr == 2){
     $s = 1;
     $s = 0 if $pflag;
@@ -113,10 +145,10 @@ if ( $npr == 0 || $npr == 2){
       }
     }
     if ( $STRT ){
-      $str = "/hpc/hub_oudenaarden/lennart/process_sam_STRT.pl -sam=".$out.".sam -barfile=".$bar." -BCset=".$BCset." -rb_len=".$rb_len." -se=".$s;
+      $str = "process_sam_STRT.pl -sam=".$out.".sam -barfile=".$bar." -BCset=".$BCset." -rb_len=".$rb_len." -se=".$s;
     }
     if ( $cel384 ){
-      $str = "/hpc/hub_oudenaarden/lennart/process_sam_cel384v2.pl -sam=".$out.".sam -barfile=".$bar." -rb_len=".$rb_len;
+      $str = "process_sam_cel384v2.pl -sam=".$out.".sam -barfile=".$bar." -rb_len=".$rb_len;
     }
     if ($gff){
 	$str = $str." -gff=".$gff;
@@ -131,5 +163,5 @@ if ( $npr == 0 || $npr == 2){
 	$str = $str." -uniq=".$uniq;
     }
     print $str."\n";
-    (system($str) == 0 or die "Could not execute ".$str."\n") if ($test == 0);
-}
+    execute(cmd=>$str, merge=>1) if ($test == 0);
+}                                       # if ( $npr == 0 || $npr == 2)
