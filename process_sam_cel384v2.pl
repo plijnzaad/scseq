7fe1f2957c8c3ad2d691ceff8accdfa063a3f235
Philip Lijnzaad <plijnzaad@gmail.com>
2016-10-12 10:39:29 +0200

    cherry-picked a6a056d6d9 (and resolved some conflicts)

--- process_sam_cel384v2.pl
+++ process_sam_cel384v2.pl
@@ -31,6 +31,7 @@ $x = 1;
 # read through sam file create a hash with all genes and cells and extract mapped reads into the hash
 
 open(IN,"<",$sam) || die "$sam: $!";
+
 SAMLINE:
 while( <IN> ) {
   chomp $_;
@@ -39,11 +40,12 @@ while( <IN> ) {
   if (substr($line1,1,2) eq "SQ" ){     # PL: keep count
     next SAMLINE;
   }
+
   if (substr($line1,1,2) eq "PG" ){
     print LOG "$line1";                 # PL:should check if it contains bwa
     next SAMLINE;
   }
-# here all the information is extracted from the sam-file line;
+
   @r1 = split(/\t/,$line1);
   my ($QNAME,$FLAG,$RNAME,$POS,$MAPQ,$CIGAR,$MRNM,$MPOS,$ISIZE,$SEQ,$QUAL,@rest)=@r1;
   my $RBC=shift(@rest);   # first is BC:Z: tag, contains barcode (== UMI + CBC)
@@ -59,16 +61,16 @@ while( <IN> ) {
     ($dum,$dum,$XA) = split(/\:/,$el) if ($el =~ /^XA\:/);
     ($dum,$dum,$X0) = split(/\:/,$el) if ($el =~ /^X0\:/);
   }
-  ### print LOG "$QNAME-$NM-$XA-$X0\n";
   ## $X0 = number of locations to which the read maps
   if ($X0 > 0){
-    $tot_map_reads++;
+    $tot_map_reads++;                   # wrong ...
+    ## $tot_map_reads += $X0; # PL
   }
   if ($X0 == 1){
     $tot_map_reads_u++;
   }
   if (exists $bar{$cellbc}){
-    if ($X0 == 1 && $FLAG != 16){
+    if ($X0 == 1 && $FLAG != 16){       # flag==16: read is reverse strand, i.e. doesn't map properly
       $tc{$RNAME}{$cellbc}{$UMI}++;
     }
   }
@@ -83,7 +85,7 @@ close(IN);
 close(LOG);
 
 
-$bn = 4 ** $rb_len;
+my $bn = 4 ** $rb_len;
 
 # create a gene array to loop through while writing the data frame;
 
@@ -98,9 +100,9 @@ $coutt   =~ s/(\.)\w+$/\.coutt\.csv/;
 $coutb   =~ s/(\.)\w+$/\.coutb\.csv/;
 $coutc   =~ s/(\.)\w+$/\.coutc\.csv/;
 
-open(OUTT, ">", $coutt) || die "$coutt: $!";
-open(OUTB, ">", $coutb) || die "$coutb: $!";
-open(OUTC, ">", $coutc) || die "$coutc: $!";
+open(OUTT, "> $coutt") || die "$coutt: $!";
+open(OUTB, "> $coutb") || die "$coutb: $!";
+open(OUTC, "> $coutc") || die "$coutc: $!";
 
 print OUTB "GENEID";
 print OUTC "GENEID";
@@ -114,22 +116,26 @@ foreach $cell (@cells){
 print OUTB "\n";
 print OUTC "\n";
 print OUTT "\n";	
+
+GENE:
 foreach $gene (sort @genes){
   print OUTB $gene;
   print OUTT $gene;
   print OUTC $gene;
+CELL:
   foreach $cell (@cells){
     $n = 0;
     $rc = 0;
+UMI:
     foreach $umi (keys %{$tc{$gene}{$cell}}){
       if ($umi =~ /N/i){
-        next;
+        next UMI;
       }
       if ($tc{$gene}{$cell}{$umi} > 0.1){
         $n++;
       }	
       $rc = $rc + $tc{$gene}{$cell}{$umi};
-    }
+    }                                   # UMI
     print OUTB "\t".$n;
     if ($n == $bn) {
       $n = $n - 0.5;
@@ -137,14 +143,14 @@ foreach $gene (sort @genes){
     print OUTC "\t".$rc;
     $trc = $trc + $rc; 
     print OUTT "\t".(-log(1 - ($n/$bn))*$bn);
-  }
+  }                                     # CELL
   print OUTT "\n";
   print OUTB "\n";
   print OUTC "\n";
-}
+}                                       # GENE
 
 $sout   =~ s/(\.)\w+$/\.sout/;
-open (SOUT, ">", $sout);
+open (SOUT, "> $sout");
 print SOUT "number of reads: ".$i."\n";
 print SOUT "number of mapped reads: ".$tot_map_reads."\n";
 print SOUT "fraction of reads mapped: ".$tot_map_reads/$i."\n";
