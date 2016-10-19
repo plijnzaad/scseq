c994642ce03ad7e3c3db878b61d885b4e4bfe0c2
Philip Lijnzaad <plijnzaad@gmail.com>
2016-10-19 12:01:46 +0200

    also added the statistics (to stderr), seems to work

--- add_bc_to_R2.pl
+++ add_bc_to_R2.pl
@@ -1,31 +1,38 @@
 #!/usr/bin/perl -w -s
-## In CELSeq2, read1 contains (in order) CBC, UMI, polyT, and read2 contains
+## In CELSeq2, read1 contains (in that order) CBC, UMI, polyT, and read2 contains
 ## the mRNA (starts of fragments). 
 ## This script puts the CBC and UMI from read1 in front of read2 and prints it
 ## to stdout.
+##
+## The current protocols have an artefact that tends to produces long ranges of polyA (and to a lesser extent polyT)
+## Specifying e.g. -A=12 will delete any occurrence of AAAAAAAAAAAA.*$ from the read (the quality line are trimmed
+## in the same way.
+## 
 
-## use strict;
+use strict;
 
-our($fastq, $rb_len, $cbc_len);
+our($fastq, $rb_len, $cbc_len, $A, $C, $G, $T);
 
 if (!($fastq)){
   die "usage: -fastq=s_R1.fastq,s_R2.fastq -rb_len=6  -cbc_len=8 [ -A=10 -C=12 -T=18 -G=14 ] > s_cbc.fastq ";
 }
 
 ## dirty hack to avoid having to use epxlicit vars
-my $polynucs ={};
+my $regexps ={};
 
 $regexps->{'A'}=$A if defined($A);
 $regexps->{'C'}=$C if defined($C);
 $regexps->{'G'}=$G if defined($G);
 $regexps->{'T'}=$T if defined($T);
-$ncleanedup={};
+my $ntrimmed={};
+my $ntrimmedtotal={};
 
 my @nucs = keys  %$regexps;
 for my $nuc (@nucs) { 
-  $re = '(' . $nuc x $regexps->{$nuc} . ".*)";
+  my $re = '(' . $nuc x $regexps->{$nuc} . ".*)";
   $regexps->{$nuc}= qr/$re/;
-  $ncleanedup->{$nuc}=0;
+  $ntrimmed->{$nuc}=0;
+  $ntrimmedtotal->{$nuc}=0;
 }
 
 die "no -rb_len specified" unless $rb_len > 0; # length of the UMI
@@ -61,15 +68,17 @@ while( not eof $IN1 and not eof $IN2) {
   }
   if ($i == 1){                   # sequence line
     $bar = substr($line1, 0, $prefix_len);
+    chomp($line2);
     for my $nuc (@nucs) { 
       if( $line2 =~ $regexps->{$nuc} ) { 
-        $newlen=length($line2) - length($1);
+        my $newlen=length($line2) - length($1);
         $trimmedlen->{$nuc}=$newlen;    # remember for the qual line
         $line2= substr($line2,0, $newlen);
-        $ncleanedup->{$nuc}++;
+        $ntrimmed->{$nuc}++;
+        $ntrimmedtotal->{$nuc} += length($1)
       }
     }
-    print  "$bar$line2";
+    print  "$bar$line2\n";
     $i++;
     next LINE;
   }
@@ -80,12 +89,13 @@ while( not eof $IN1 and not eof $IN2) {
   }
   if ($i == 3){                   # line with Phred qualities
     my $qual= $barcode_quality x $prefix_len;
+    chomp($line2);
     for my $nuc (@nucs) {               # trim qual line if seqline was
       if(exists($trimmedlen->{$nuc})) { 
         $line2= substr($line2,0, $trimmedlen->{$nuc});
       }
     }
-    print  "$qual$line2";
+    print  "$qual$line2\n";
     $i = 0;
     $trimmedlen={};
   }
@@ -94,4 +104,7 @@ while( not eof $IN1 and not eof $IN2) {
 close $IN1 || die "$fastq[0]: $!";
 close $IN2 || die "$fastq[1]: $!";
 
-
+for my $nuc (@nucs) { 
+  warn "trimmed $ntrimmed->{$nuc} poly${nuc}'s from the reads (totalling $ntrimmedtotal->{$nuc} nucleotides)\n"
+      if exists($ntrimmed->{$nuc}) && $ntrimmed->{$nuc} > 0;
+}
