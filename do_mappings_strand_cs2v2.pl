2601ea7ece508650c37f9ed6885409b9dd0cb9a4
Philip Lijnzaad <plijnzaad@gmail.com>
2016-10-20 14:29:11 +0200

    sanitized code, now using strict. No more uninitialized values either

--- do_mappings_strand_cs2v2.pl
+++ do_mappings_strand_cs2v2.pl
@@ -2,8 +2,12 @@
 use tools;
 use Carp;
 use File::Basename;
+use strict;
 
-$LIST_SEPARATOR=" ";                    # for interpolating arrays inside strings (default anyway)
+$,=" ";         # for interpolating arrays inside strings (default anyway)
+
+our($r, $f1, $f2, $out, $bwaparams, $outdir, $t, $ind, $q, $aln_n, $aln_k, $l,
+    $BR, $i, $npr, $nsam, $bar, $rb_len, $cbc_len, $trim, $allow_mm, $test);
 
 if (!($r && $f1 && $out && $t)){
   confess "Usage:  $0 -r=REFERENCE     \
@@ -25,14 +29,15 @@ if (!($r && $f1 && $out && $t)){
                  -bar=cel-seq_barcodes.csv    \
                  -rb_len= length of UMI (default = 6)  \
                  -cbc_len= length of cellseq2 barcode (default: 8) \
-                 -trim='A12,T=14' (passed to add_bc_to_R2.pl for trimming) \
+                 -trim='A12,T=14' (optional, passed to add_bc_to_R2.pl for trimming) \
+                 -allow_mm=N (optional, passed to process_sam_cel384v2, allows N mismatches in cell bar codes) \
                  -test=0 or 1 (latter runs in test mode, doesn't call external programs) \
 ";
 }
 
 # hard coded:
-$n = 100; # maximal number of unpaired reads to output in XA tag
-$N = 100; # maximal number of paired reads to output in XA tag
+my $n = 100; # maximal number of unpaired reads to output in XA tag
+my $N = 100; # maximal number of paired reads to output in XA tag
 
 $q = 0 if !$q; # base quality cutoff for trimming
 $l = 200 if !$l; # seed length
@@ -53,6 +58,8 @@ $aln_k = 2 if !$aln_k; # edit distance in seed
 
 $bwaparams=" -q $q -n $aln_n -k $aln_k -l $l " unless $bwaparams;
 
+$allow_mm="" unless $allow_mm;
+
 $test = 0 if !$test;
 if ($outdir){
   makedir($outdir);
@@ -62,7 +69,7 @@ if ($outdir){
 confess "reference genome $r not found" unless -f $r;
 
 if ($i){
-  $str = "bwa index -a $ind $r";
+  my $str = "bwa index -a $ind $r";
   print $str."\n";
   execute(cmd=>$str, merge=>1) if ($test == 0);
 }
@@ -77,22 +84,21 @@ if ( $ext =~ /\.gz/ ) {
   $gzip = " gzip ";
 }
 
-$sai = "$outdir/$name.sai"; 
-
 if ( $npr != 2 ) {                       # npr is 0 or 1: do mapping
-  if ($F[$i] =~ /txt/)  { die "obsolete, see before commit 61a2fce50246ce47 (2016-10-11 15:10:00)";}
   if ( -f $cbc  ) { 
     print "*** Seeing file $cbc, not running add_bc_to_R2.pl to re-create it\n";
   } else { 
     $trim = "-trim=$trim" if defined($trim);
-    $str = "add_bc_to_R2.pl -fastq=$f1,$f2 -rb_len=$rb_len -cbc_len=$cbc_len $trim | $gzip > $cbc ";
+    my $str = "add_bc_to_R2.pl -fastq=$f1,$f2 -rb_len=$rb_len -cbc_len=$cbc_len $trim | $gzip > $cbc ";
     print $str."\n";
     execute(cmd=>$str, merge=>0) if ($test == 0);
     check_filesize(file=>$cbc, minsize=>1000);
   }
 
-  $B = $BR;
-  $str = "bwa aln -B $B -t $t $bwaparams $r $cbc > $sai";
+  my $B = $BR;
+  my $sai = "$outdir/$name.sai"; 
+
+  my $str = "bwa aln -B $B -t $t $bwaparams $r $cbc > $sai";
 ##  $str = "bwa aln -B $B -q $q -n $aln_n -k $aln_k -l $l -t $t $r $cbc > $sai";
   print $str."\n";
   execute(cmd=>$str, merge=>0) if ($test == 0);
@@ -109,7 +115,7 @@ if ( $npr != 2 ) {                       # npr is 0 or 1: do mapping
 
 if ( $npr == 0 || $npr == 2){
   ## if ( $STRT ) # unknown, see before commit 61a2fce50246ce47 (2016-10-11 15:10:00)
-  $str = "process_sam_cel384v2.pl -sam=$out.bam -barfile=$bar -rb_len=$rb_len -cbc_len=$cbc_len";
+  my $str = "process_sam_cel384v2.pl -sam=$out.bam -barfile=$bar -rb_len=$rb_len -cbc_len=$cbc_len $allow_mm";
   print $str."\n";
   execute(cmd=>$str, merge=>1) if ($test == 0);
 }                                       # if ( $npr == 0 || $npr == 2)
