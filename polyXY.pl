cd37e15c1fa9884768573f828a00351a628e97bb
Philip Lijnzaad <plijnzaad@gmail.com>
2016-10-24 18:18:45 +0200

    polyXY, seems to work

---
+++ polyXY.pl
@@ -0,0 +1,46 @@
+#!/bin/env perl
+
+### find stretches of polyXY 's (e.g. AAAACCCCC but also CCCCCAAAAA, for
+### all combinations of two diff nucleotides Needs (on stdin) sequences in
+### tab format, i.e.  ID \t ACACACTCAGCGAGCTCCAC...ACACCACTGTT\n
+
+use strict;
+
+use Math::Combinatorics;
+use Regexp::Optimizer;
+
+my $ndiff=2;
+my @combs=combine($ndiff, qw(A C T G));
+@combs =  map { permute @$_ } @combs;
+
+my $REs={};
+
+my $o=Regexp::Optimizer->new;
+
+my ($min)=shift;
+
+die "Usage: $0:  ... | $0   N " unless $min > 0;
+
+for my $comb (@combs) { 
+   my $name=join("_",@$comb)."_";
+  my $quant="{$min,}"; 
+  my $RE=(join($quant, @$comb)). "$quant";
+  $REs->{$name}=  $o->optimize(qr/$RE/i);
+}
+
+my @sids=sort keys %$REs;
+my $hits={};
+
+while(<>) {
+  my ($id, $seq)=split("\t", $_);
+  s/N/A/g;
+  for my $sid (@sids) { 
+    $hits->{$sid} += ($seq =~ $REs->{$sid});
+  }
+}
+
+for my $sid (@sids) { 
+  my $n=$hits->{$sid};
+  $n=0 unless $n;
+  print "$sid\t$n\n";
+}
