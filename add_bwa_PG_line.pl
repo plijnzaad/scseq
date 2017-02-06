5c701f42f8a1d755cad7b6eff5babec2e8eb4487
Philip Lijnzaad <plijnzaad@gmail.com>
2017-02-06 11:33:11 +0100

    more flexible arg checking

--- add_bwa_PG_line.pl
+++ add_bwa_PG_line.pl
@@ -5,9 +5,11 @@ use strict;
 
 my $usage = "Usage (e.g.) :  bwa samse -n N reference.fa output.sai input.fastq.gz  |  add_bwa_PG_line.pl  bwa aln -B 0 -t 4  -q 40 -n 0.02 -k 2 -l 200 reference.fa input.fastq.gz |  samtools view -h -b -   > file.bam";
 
-die $usage unless @ARGV >= 3 && $ARGV[0] eq 'bwa';
-my @args = @ARGV; 
-@ARGV=();
+# allow both 'bwa aln ref fastq' and list ('bwa', 'aln', 'theref', 'thefastq')
+my @args = split(/\s+/, join(" ", @ARGV));
+@ARGV=();                               # otherwise args are considered files to concatenate as STDIN
+
+die $usage unless @args >= 4 && $args[0] eq 'bwa';
 
 while(<>) { 
   if( /^\@PG.*bwa.*sam[ps]e/) { 
